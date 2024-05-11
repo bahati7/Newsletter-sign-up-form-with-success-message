de5951192a2f695b556103d4e1d86d428863a334
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-05-11 12:52:35 +0200

    conditional display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
+import "./App.css";
+import Forminput from "./components/Forminput";
+import Formsuccess from "./components/Formsuccess";
 
 function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleFormSubmit = () => {
+    setIsSubmitted(true); // Set submitted state to true on successful form submission
+  };
+
+  const handleReturnToForm = () => {
+    setIsSubmitted(false); // Reset submitted state
+   // setIsFormVisible(true); // Make form visible again
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {isSubmitted ? null : <Forminput onSubmitSuccess={handleFormSubmit} />}  {/* Conditionally render EmailForm */}
+      {isSubmitted && <Formsuccess onReturnToForm={handleReturnToForm}/> }
+      
     </div>
   );
 }
